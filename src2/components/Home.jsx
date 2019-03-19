de11b820ee60238c74c41ed42294528db56e3ba7
kevin1105 <31319917+kevin1105@users.noreply.github.com>
2019-03-19 00:22:18 -0700

    Update Home.jsx

--- src2/components/Home.jsx
+++ src2/components/Home.jsx
@@ -12,7 +12,8 @@ import { Switch } from 'react-router'
 import TextField from 'material-ui/TextField';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Profile from './Profile'
-
+import { Column, Row } from 'simple-flexbox';
+ 
 function App() {
   return (
     <Router>
@@ -48,47 +49,39 @@ function Topics({ match }) {
     );
 }
 
-function goHome() {
-    console.log('/');
-    return (
-        <link to="/">jj
-        </link>
-    )
-}
-
 const fakeAuth = {
-    isAuthenticated: false,
-    authenticate(cb) {
-      this.isAuthenticated = true;
-      setTimeout(cb, 100); // fake async
-    },
-    signout(cb) {
-      this.isAuthenticated = false;
-      setTimeout(cb, 100);
-    }
-  };
-  
-  const AuthButton = withRouter(
-    ({ history }) =>
-      fakeAuth.isAuthenticated ? (
-        <p>
-          You are logged on !{" "}
-          <button
-            onClick={() => {
-              fakeAuth.signout(() => history.push("/"));
-            }}
-          >
-            Sign out
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    fakeAuth.isAuthenticated ? (
+      <p>
+        You are logged on !{" "}
+        <button
+          onClick={() => {
+            fakeAuth.signout(() => history.push("/"));
+          }}
+        >
+          Sign out
           </button>
-        </p>
-      ) : (
+      </p>
+    ) : (
         <p>
-        <br />
-        User: Kevin, Pass: qwerty
+          <br />
+          User: Kevin, Pass: qwerty
         </p>
-        
+
       )
-  );
+);
   
 class Login extends Component {
     state = { isLogin: false };
@@ -144,6 +137,7 @@ class Login extends Component {
                 <button onClick={this.login}>Log in</button>
             </div>
             </MuiThemeProvider>
+            
         );
         
     }
@@ -154,7 +148,7 @@ const styles = {};
 // paddingTop: "50px",
 
 styles.mainButton = {
-    position: "absolute",
+    // position: "absolute",
     fontSize: "50px",
     color: "red"
 }
@@ -168,21 +162,21 @@ styles.homeButton = {
 }
 
 styles.loginButton = {
-    position: "absolute",
-    left: "25%",
+    // position: "absolute",
+    // left: "25%",
     fontSize: "20px",
     color: "green",
-    paddingTop: "25px"
+    // paddingTop: "25px",
 }
 
 styles.signupButton = {
-    position: "absolute",
-    left: "30%",
+    // position: "absolute",
+    // left: "30%",
     fontSize: "20px",
     color: "green",
-    paddingTop: "1px"
+    // paddingTop: "1px"
 }
-
+ 
 styles.welcome = {
   paddingTop: "50px"
 }
@@ -190,35 +184,39 @@ styles.welcome = {
 styles.placeCenter = {
   position: "absolute",
   left: "40%",
-  paddingTop: "200px"
+  paddingTop: "150px"
 }
 
 styles.profileButton = {
-  position: "absolute",
-  left: "36%",
+  // position: "absolute",
+  // left: "36%",
   fontSize: "20px",
   color: "green",
-  paddingTop: "1px"
+  // paddingTop: "1px"
 }
 
 function Header() {
   return (
     <ul>
-      <div>
-        <button class="btn" style={styles.mainButton}
-          onClick={goHome}> YeetFood </button>
-        <Link to="/" style={styles.homeButton} >Home</Link>
-      </div>
-      <div>
-        <Link to="/login" style={styles.loginButton}>Login</Link>
-      </div>
-      <br />
-      <div>
-        <Link to="/signup" style={styles.signupButton}>Sign Up</Link>
-      </div>
-      <div>
-        <Link to="/profile" style={styles.profileButton}>Profile</Link>
-      </div>
+      <Column flexGrow={1}>
+        <Row>
+          <Link to="/" >
+            <button class="btn" style={styles.mainButton}
+            > YeetFood </button>
+          </Link>
+        </Row>
+        <Row horizontal='start'>
+          <Column flexGrow={0.1} horizontal='space-between'>
+            <Link to="/login" style={styles.loginButton}>Login</Link>
+          </Column>
+          <Column flexGrow={0.1} horizontal='space-between'>
+            <Link to="/signup" style={styles.signupButton}>Sign Up</Link>
+          </Column>
+          <Column flexGrow={0.1} horizontal='space-between'>
+            <Link to="/profile" style={styles.profileButton}>Profile</Link>
+          </Column>
+        </Row>
+      </Column>
     </ul>
   );
 }
