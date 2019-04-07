e7a90da05d8073cd0a27a4966dd8845cdf6ef703
kevin1105 <31319917+kevin1105@users.noreply.github.com>
2019-04-07 12:16:25 -0700

    Create HomeHelper.jsx

---
+++ src2/components/HomeHelper.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import axios from "axios";
+import TextField from "material-ui/TextField";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  paper: {
+    marginRight: theme.spacing.unit * 2
+  }
+});
+
+const styles2 = {};
+
+styles2.button = {
+    position: "absolute",
+    fontSize: "20px",
+    color: "red",
+    padding: 10,
+    fontWeight: "bold",
+    border: "transparent"
+}
+
+class Dropdown extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleToggle = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  handleClose = event => {
+    if (this.anchorEl.contains(event.target)) {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div>
+        <Button
+          buttonRef={node => {
+            this.anchorEl = node;
+          }}
+          aria-owns={open ? "menu-list-grow" : undefined}
+          aria-haspopup="true"
+          onClick={this.handleToggle}
+          style={styles2.button}
+        >
+          Options
+        </Button>
+        <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              id="menu-list-grow"
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom"
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={this.handleClose}>
+                  <MenuList>
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    );
+  }
+}
+
+Dropdown.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default Dropdown;
