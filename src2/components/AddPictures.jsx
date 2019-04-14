fa6a367497bb2e36a4b5eedd261e3525df7b3cc5
kevin1105 <31319917+kevin1105@users.noreply.github.com>
2019-04-14 05:13:48 -0700

    Update AddPictures.jsx

--- src2/components/AddPictures.jsx
+++ src2/components/AddPictures.jsx
@@ -1,33 +1,43 @@
 import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import ImageUploader from 'react-images-upload';
+import axios from "axios";
 
 class AddPic extends React.Component {
 
-    constructor(props) {
-        super(props);
-         this.state = { pictures: [] };
-         this.onDrop = this.onDrop.bind(this);
-    }
- 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
+  state = {
+    selectedFile: null
+  };
+
+  fileSelectedHandler = event => {
+    console.log(event.target.files[0]);
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    var CLOUDINARY_UPLOAD_PRESET = 'k3nwpe0l';
+    fd.append('file', this.state.selectedFile);
+    fd.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+    axios.post("https://api.cloudinary.com/v1_1/fooddelivery/image/upload", fd)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
         });
-    }
- 
-    render() {
-        return (
-            <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880}
-            />
-        );
-    }
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="file" onChange={this.fileSelectedHandler} />
+        <button onClick={this.fileUploadHandler}> Upload </button>
+      </div>
+    );
+  }
 }
 
 export default AddPic;
